65b44818157b78bcb3b8bff941341984e21d81d3
Alexey Pisarenko <alp@itcrimea.com>
2014-03-21 21:31:36 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ticketstore::Application.config.secret_key_base = '85e66e8ff3418bdd0e16ec53506b45ab45c830e71aa207b53deb42e5bc27748680082e41aad908fb166fb14496d346bbc4be5b585db479802df51a92db94fa88'
