98e56006b5e55a55f710c0f347b957cfaab40507
Alexey Pisarenko <alp@itcrimea.com>
2014-03-21 21:56:23 +0200

    Fixed name in rspec for User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,6 @@ YAML.load(ENV['ROLES']).each do |role|
   puts 'role: ' << role
 end
 puts 'DEFAULT USERS'
-user = User.create! :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
+user = User.create! :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+puts 'user: ' << user.email
 user.add_role :admin
